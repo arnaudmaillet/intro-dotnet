a5036f58877c9ccc4d9ac7608aef9934dcd27ff2
Adrienne Tchamgoue <adrienne.tchamgoue@gmail.com>
2023-04-20 12:12:53 +0200

    afficher les infos d'une personne

--- src/WebApplication1/Default.aspx
+++ src/WebApplication1/Default.aspx
@@ -2,41 +2,18 @@
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
 
-    <div class="jumbotron">
-        <h1>ASP.NET</h1>
-        <p class="lead">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript.</p>
-        <p><a href="http://www.asp.net" class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
-    </div>
-
-    <div class="row">
-        <div class="col-md-4">
-            <h2>Getting started</h2>
-            <p>
-                ASP.NET Web Forms lets you build dynamic websites using a familiar drag-and-drop, event-driven model.
-            A design surface and hundreds of controls and components let you rapidly build sophisticated, powerful UI-driven sites with data access.
-            </p>
-            <p>
-                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301948">Learn more &raquo;</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <h2>Get more libraries</h2>
-            <p>
-                NuGet is a free Visual Studio extension that makes it easy to add, remove, and update libraries and tools in Visual Studio projects.
-            </p>
-            <p>
-                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301949">Learn more &raquo;</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <h2>Web Hosting</h2>
-            <p>
-                You can easily find a web hosting company that offers the right mix of features and price for your applications.
-            </p>
-            <p>
-                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301950">Learn more &raquo;</a>
-            </p>
-        </div>
-    </div>
+    <table style="width: 80%;">
+        <caption>Exo CRUD</caption>
+        <tr>
+            <th style="margin:0 1rem"> Last Name </th>
+            <th style="margin:0 1rem"> First Name </th>
+            <th style="margin:0 1rem"> Age </th>
+        </tr>
+        <tr>
+            <td> <asp:Label ID="LastName" runat="server"></asp:Label> </td>
+            <td> <asp:Label ID="FirstName" runat="server"></asp:Label> </td>
+            <td> <asp:Label ID="Age" runat="server"></asp:Label> </td>
+        </tr>
+    </table>
 
 </asp:Content>
--- src/WebApplication1/Site.Master.cs
+++ src/WebApplication1/Site.Master.cs
@@ -9,9 +9,127 @@ namespace WebApplication1
 {
     public partial class SiteMaster : MasterPage
     {
+        private PersonRepository listPerson;
         protected void Page_Load(object sender, EventArgs e)
         {
+            // listPerson = new PersonRepository();
 
+            // var listAllPerson = listPerson.GetAll();
+            // listView1.Columns.Add("ID", 200);
+            // listView1.Columns.Add("FirstName", 200);
+            // listView1.Columns.Add("LastName", 200);
+            // listView1.Columns.Add("Age", 200);
+
+            // LoadData(listAllPerson);
+        }
+
+        private void LoadData(IEnumerable<Person> list)
+        {
+            // foreach (Person person in list)
+            // {
+            //     ListViewItem item = new ListViewItem(person.PersonId.ToString());
+            //     item.SubItems.Add(person.FirstName);
+            //     item.SubItems.Add(person.LastName);
+            //     item.SubItems.Add(person.Age.ToString());
+            //     listView1.Items.Add(item);
+            // }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // listPerson.Create("Corentin", "Lebarilier", 21);
+
+
+            // foreach (Person person in listPerson.GetAll())
+            // {
+            //     Debug.WriteLine(person.FirstName);
+            // }
+
+            // LoadData(listPerson.GetAll());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // if (listView1.SelectedItems.Count > 0)
+            // {
+            //     ListViewItem selectedItem = listView1.SelectedItems[0];
+            //     var selectedValue = selectedItem.Text;
+            //     listPerson.Delete(selectedValue);
+            // }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+// using DataProvider;
+// using System.Diagnostics;
+// using System.Windows.Forms;
+
+// namespace WinFormsApp
+// {
+//     public partial class Form1 : Form
+//     {
+//         private PersonRepository listPerson;
+//         public Form1()
+//         {
+//             InitializeComponent();
+//         }
+
+//         private void Form1_Load(object sender, EventArgs e)
+//         {
+//             listPerson = new PersonRepository();
+
+//             var listAllPerson = listPerson.GetAll();
+//             listView1.Columns.Add("ID", 200);
+//             listView1.Columns.Add("FirstName", 200);
+//             listView1.Columns.Add("LastName", 200);
+//             listView1.Columns.Add("Age", 200);
+
+
+
+//             LoadData(listAllPerson);
+//         }
+
+//         private void LoadData(IEnumerable<Person> list)
+//         {
+//             foreach (Person person in list)
+//             {
+//                 ListViewItem item = new ListViewItem(person.PersonId.ToString());
+//                 item.SubItems.Add(person.FirstName);
+//                 item.SubItems.Add(person.LastName);
+//                 item.SubItems.Add(person.Age.ToString());
+//                 listView1.Items.Add(item);
+//             }
+//         }
+
+//         private void button1_Click(object sender, EventArgs e)
+//         {
+//             listPerson.Create("Corentin", "Lebarilier", 21);
+
+
+//             foreach (Person person in listPerson.GetAll())
+//             {
+//                 Debug.WriteLine(person.FirstName);
+//             }
+
+//             LoadData(listPerson.GetAll());
+//         }
+
+//         private void button2_Click(object sender, EventArgs e)
+//         {
+//             if (listView1.SelectedItems.Count > 0)
+//             {
+//                 ListViewItem selectedItem = listView1.SelectedItems[0];
+//                 var selectedValue = selectedItem.Text;
+//                 listPerson.Delete(selectedValue);
+//             }
+//         }
+//     }
+// }
