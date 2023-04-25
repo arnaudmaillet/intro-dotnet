b76932b441798edc4783cdcc3ca71c91ec1139bf
Adrienne Tchamgoue <adrienne.tchamgoue@gmail.com>
2023-04-25 10:33:56 +0200

    aspnet mvc for list and delete

--- src/WebApplication1/Controllers/HomeController.cs
+++ src/WebApplication1/Controllers/HomeController.cs
@@ -3,27 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DataProvider;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        private PersonRepository listUser = new PersonRepository();
+
         public ActionResult Index()
         {
-            return View();
+            IEnumerable<Person> tableOfPeople = listUser.GetAll();
+            return View(tableOfPeople);
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
