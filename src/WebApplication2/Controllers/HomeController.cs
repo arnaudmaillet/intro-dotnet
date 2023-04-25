793296bd902d145ff356b11ebbf64c11ce189567
Adrienne Tchamgoue <adrienne.tchamgoue@gmail.com>
2023-04-25 12:20:53 +0200

    Lister les personnes

--- src/WebApplication2/Controllers/HomeController.cs
+++ src/WebApplication2/Controllers/HomeController.cs
@@ -6,11 +6,15 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApplication2.Models;
+using DataProvider;
 
 namespace WebApplication2.Controllers
 {
     public class HomeController : Controller
+
     {
+        private PersonRepository listUser = new PersonRepository();
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -20,7 +24,30 @@ namespace WebApplication2.Controllers
 
         public IActionResult Index()
         {
+            
+            IEnumerable<Person> data = listUser.GetAll();
+
+            return View(data);
+        }
+
+        public async Task<IActionResult> Supprimer(int id)
+        {
+            /*var person = await _context.Person.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }*/
+
             return View();
+        }
+
+        [HttpPost, ActionName("Supprimer")]
+        public async Task<IActionResult> ConfirmerSupprimer(int id)
+        {
+            /*var person = await _context.Person.FindAsync(id);
+            _context.Produits.Remove(produit);
+            await _context.SaveChangesAsync();*/
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
